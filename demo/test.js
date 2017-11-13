5473fdf69767a601aa7abb05473cfa5ff263d820
ArthurFree <lc940401@gmail.com>
2017-11-13 16:39:59 +0800

    test

--- demo/test.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-// let pathname = path.resolve(__dirname, '..', 'src/index');
-
-let pathname = path.join(__dirname, '..', 'dist', 'static');
-console.log('--- pathname ---', pathname);
-
